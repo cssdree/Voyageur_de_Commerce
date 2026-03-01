e731f29f511dd817c2b9ba352b744f7a6f11a852
cassandre <github@cassandre.klo.re>
2026-03-01 11:55:45 +0100

    test interdiction revisiter ville

--- jeu.py
+++ jeu.py
@@ -2,17 +2,21 @@ import math
 
 
 class Jeu():
-    def __init__(self, nbjoueur, villes):
+    def __init__(self, nbjoueur, villes, depart):
         self.nbjoueur = nbjoueur
         self.villes = villes
-        self.parcours = []
+        self.parcours = [depart]
+        self.visitees = {depart}
 
-    def ChoixVille(self, joueur, idville):
-        self.parcours.append(self.villes[idville])
+    def ChoixVille(self, idjoueur, idville):
+        if idville in self.visitees:
+            raise PermissionError("Ville déjà visitées")
+        self.parcours.append(idville)
+        self.visitees.add(idville)
 
-    def Score(self, joueur):
+    def Score(self, idjoueur):
         score = 0
         for v in range(1, len(self.parcours)):
-            distance = math.dist(self.parcours[v], self.parcours[v-1])
+            distance = math.dist(self.villes[self.parcours[v]], self.villes[self.parcours[v-1]])
             score += distance
         return score
--- test_jeu.py
+++ test_jeu.py
@@ -4,13 +4,23 @@ import math
 
 
 class TestJeu(unittest.TestCase):
-    def test_partie_1_joueur(self):
-        jeu = Jeu(nbjoueur=1, villes=[(0, 0), (1,1), (6,6)])
-        jeu.ChoixVille(joueur=1, idville=1)
-        jeu.ChoixVille(joueur=1, idville=0)
-        self.assertEqual(math.sqrt(2), jeu.Score(joueur=1))
-        jeu.ChoixVille(joueur=1, idville=2)
-        self.assertEqual(7*math.sqrt(2), jeu.Score(joueur=1))  # add assertion here
+    def test_partie_un_joueur(self):
+        jeu = Jeu(nbjoueur=1, villes={0:(0, 0), 1:(1,1), 2:(6,6)}, depart=0)
+        jeu.ChoixVille(idjoueur=1, idville=1)
+        self.assertEqual(math.sqrt(2), jeu.Score(idjoueur=1))
+        jeu.ChoixVille(idjoueur=1, idville=2)
+        self.assertEqual(6*math.sqrt(2), jeu.Score(idjoueur=1))
+
+    def test_interdiction_revisiter_ville(self):
+        jeu = Jeu(nbjoueur=1, villes={0:(0, 0), 1:(1,1), 2:(6,6)}, depart=0)
+        jeu.ChoixVille(idjoueur=1, idville=1)
+        with self.assertRaises(PermissionError):
+            jeu.ChoixVille(idjoueur=1, idville=1)
+        jeu.ChoixVille(idjoueur=1, idville=2)
+        with self.assertRaises(PermissionError):
+            jeu.ChoixVille(idjoueur=1, idville=2)
+
+    #def test_partie_deux_joueur(self):
 
 
 if __name__ == '__main__':

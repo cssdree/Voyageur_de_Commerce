8b5615e7d3dc4beae08b009bad4529c7da2ad1e1
cassandre <github@cassandre.klo.re>
2026-03-01 19:10:59 +0100

    test parcours recursif

---
+++ Tests/test_heuristiques.py
@@ -0,0 +1,24 @@
+from heuristiques import *
+from villes import Villes
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_heuristique_gloutonne(self):
+        villes = Villes(dict_villes={0:(1,1), 1:(1,4), 2:(6,6), 3:(8,9)}, depart=0)
+        self.assertEqual(HeuristiqueGloutonne(villes), [0, 1, 2, 3, 0])
+        villes = Villes(dict_villes={0: (1, 1), 1: (1, 4), 2: (6, 6), 3: (8, 9)}, depart=1)
+        self.assertEqual(HeuristiqueGloutonne(villes), [1, 0, 2, 3, 1])
+
+
+    def test_parcours_recursif(self):
+        villes = Villes(dict_villes={0:(5,1), 1:(5,2), 2:(5,3), 3:(5,4)}, depart=0)
+        parcours_recursif = ParcoursRecursif(villes)
+        self.assertEqual(parcours_recursif.ResultatRecursif(), [0, 1, 2, 3, 0])
+        villes = Villes(dict_villes={0:(2,2), 1:(4,2), 2:(4,4), 3:(2,4)}, depart=0)
+        parcours_recursif = ParcoursRecursif(villes)
+        self.assertEqual(parcours_recursif.ResultatRecursif(), [0, 1, 2, 3, 0])
+
+
+if __name__ == '__main__':
+    unittest.main()
